bf8cfe1c44511aafdf11d6146f3ce0bb69f9d75f
Richard Irving <irvnet@gmail.com>
2022-02-11 15:03:26 -0500

    update port list in sg

--- provision/sg.tf
+++ provision/sg.tf
@@ -10,6 +10,22 @@ resource "aws_security_group" "allow-ssh" {
       cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # TFE access (HTTP; redirects to HTTPS)
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # TFE access (HTTPS)
+  ingress {
+      from_port = 443
+      to_port = 443
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
       from_port = 22
       to_port = 22
