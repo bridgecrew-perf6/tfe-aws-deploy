6ddd4cd7e664440fe92e0ab87b9b21546b701f1d
Richard Irving <irvnet@gmail.com>
2022-02-11 20:33:28 -0500

    add docker install and tidbits to validate the script ran

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-echo 'prepping web server image... '
-sudo apt-get -y update
+echo '** -> prepping TFE image... '
+sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y autoremove
-sudo apt-get -y install unzip curl
+sudo apt-get -y install unzip curl 
+
+# test marker
+echo "TFE Server 01" > ~/tfe.info
+cat /etc/os-release
+lsb_release -a
+hostnamectl
+cowsay "getting it together..."
 
 # install docker
-sudo apt install containerd
+sudo apt-get -y install containerd
 containerd --version
 runc --version
 
+sudo apt-get update && sudo apt-get -y upgrade 
+
 # TFE install
-curl https://install.terraform.io/ptfe/stable | sudo bash
+echo "curl https://install.terraform.io/ptfe/stable | sudo bash" > ~/script.sh
+chmod +x ~/script.sh
